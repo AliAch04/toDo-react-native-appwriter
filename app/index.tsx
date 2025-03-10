22026f8ac7cf84ed11fbfb01e810f0d59d1e9bba
AliAch04 <ali.jallousy12@gmail.com>
2025-03-10 17:55:41 +0000

    Reorganize and clean up Expo app code and fixing some bugs

--- app/index.tsx
+++ app/index.tsx
@@ -1,39 +1,49 @@
-import { Text, View, SafeAreaView, FlatList } from "react-native";
+import { Text, View, SafeAreaView, FlatList, StyleSheet } from "react-native";
 import { useEffect, useState } from "react";
-import {config, database} from "@/lib/appwrite"
+import { config, database } from "@/lib/appwrite";
+import ListItem from "./components/ListItem";
+import TestComponent from "./components/TestComponent";
 
 export default function Index() {
-  const [tasks, setTasks] = useState([])
-  const [error, setError] = useState(null)
+  const [tasks, setTasks] = useState([]);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    init()
-  }, [])
-
-  const init = async () => {
-    getData()
-  }
+    getData();
+  }, []);
 
   const getData = async () => {
     try {
-      const {documents, total} = await database.listDocuments(config.db, config.col.tasks)
-      setTasks(documents)
+      const { documents } = await database.listDocuments(config.db, config.col.tasks);
+      setTasks(documents);
       console.log(documents);
-      
-      
     } catch (error) {
-      setError(error)
+      setError(error);
     }
-  }
+  };
 
   return (
     <SafeAreaView>
-      {error && (<Text>{JSON.stringify(error)}</Text>)}
-      <Text>Todo List: </Text>
-      <FlatList 
-        data={tasks}
-        renderItem={({item}) => <Text>{item.body}</Text>}
-      />
+      <View style={styles.container}>
+        {error && <Text>{JSON.stringify(error)}</Text>}
+        <TestComponent style={styles.headline} fontSize={22}>
+          ToDo List:
+        </TestComponent>
+        <FlatList
+          data={tasks}
+          renderItem={({ item }) => <ListItem task={item} />}
+          keyExtractor={(item) => item.$id}
+        />
+      </View>
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: 20,
+  },
+  headline: {
+    paddingVertical: 20,
+  },
+});
